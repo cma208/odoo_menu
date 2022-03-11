f85675883db735e2e3f7efdf2085bdf716d10bea
Carlos <carlos.machicado.123@hotmail.com>
2022-03-10 19:02:31 -0500

    Error 2

--- __manifest__.py
+++ __manifest__.py
@@ -3,14 +3,14 @@
     'summary': "Check Our New Menu",
     'autor': "Carlos Machicado",
     'website': "http://www.example.com",
-    'category': 'Restaurant',
+    'category': 'Accounting',
     'version': '14',
     'depends': ['base', 'sale'],
     'data': [
-        'security/groups.xml',
+        #'security/groups.xml',
         'security/ir.model.access.csv',
-        'data/data.dishes.csv',
+        #'data/data.dishes.csv',
         'views/menu_dishes.xml',
     ],
-    'demo': ['demo.xml'],
+    #'demo': ['demo.xml'],
 }
--- models/menu_dishes.py
+++ models/menu_dishes.py
@@ -2,8 +2,9 @@ from odoo import models, fields, api
 
 
 class MenuDishes(models.Model):
-    _name='menu.dishes'
-    _description='Carito Ardiles Menu'
-    _order='name, cost_price'
-    cost_price=fields.Float('Dish Price', digits='Dish Price')
-    notes=fields.Text('Dish Description')
+    _name = 'menu.dishes'
+    _description = 'Carito Ardiles Menu'
+    _order = 'name, cost_price'
+    name = fields.Char('Dish Name')
+    cost_price = fields.Float('Dish Price', digits='Dish Price')
+    notes = fields.Text('Dish Description')
